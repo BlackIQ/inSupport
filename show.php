c915e8eab1df73335448a2bec85cc59ab35f395e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-26 15:28:35 +0430

    Fix title

--- show.php
+++ show.php
@@ -37,7 +37,7 @@ $username = $rowuser['fname'];
 <html lang="en">
 
 <head>
-    <title>Narbon Support | Index</title>
+    <title>Narbon Support | Ticket <?php echo $code; ?></title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
