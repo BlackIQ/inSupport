eb513714cd7fa0ad8bc68405d1481427fcb579b2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-26 15:28:43 +0430

    Fix title

--- tickets.php
+++ tickets.php
@@ -25,7 +25,7 @@ $result = mysqli_query($conn, $sql);
 <html lang="en">
 
 <head>
-    <title>Narbon Support | Index</title>
+    <title>Narbon Support | Tickets</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
