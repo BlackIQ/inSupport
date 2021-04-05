104c4fec59cdd0e857c57141d9d3a1fcfc3b1185
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-05 11:38:44 +0430

    Create files

---
+++ panels/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container-fluid">
+        <p class="copyright">&copy; <?php echo date("Y"); ?> <a href="#">Narbon</a>. All Rights Reserved.</p>
+    </div>
+</footer>
---
+++ panels/nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="brand">
+        <span><b><span class="text-primary">Narbon</span> Support</b></span>
+    </div>
+    <div class="container-fluid">
+        <div class="navbar-btn">
+            <button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
+        </div>
+        <div class="navbar-btn navbar-btn-right">
+            <a class="btn btn-danger" href="#"><i class="fa fa-sign-out"></i> <span>Logout</span></a>
+        </div>
+    </div>
+</nav>
