0a804e7b1665cc14b5a6f6f93dc2df7b8ad0a51e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-07 00:53:35 +0430

    Include data file and fixed logout link

--- account/logout.php
+++ account/logout.php
@@ -1,21 +1,7 @@
 <?php
 
-session_start();
-
-$server = "localhost";
-$user = "narbon";
-$passwd = "narbon";
-$db = "narbonn";
-
-$conn = mysqli_connect($server, $user, $passwd, $db);
-
-$getip = "SELECT * FROM development";
-$res = mysqli_query($conn, $getip);
-
-while ($row = mysqli_fetch_assoc($res)) {
-    $ip = $row['ip'];
-}
+include("data.php");
 
 session_destroy();
 
-header("Location: http://$ip/Narbon");
+header("Location: http://$ip/NarbonSupport");
