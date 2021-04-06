dd5345c1efeef3b1dfb2088e0ef32ff060b6e247
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-07 00:46:09 +0430

    Fix links

--- panels/silebar.php
+++ panels/silebar.php
@@ -1,6 +1,23 @@
 <?php
 
+session_start();
+
 function index() {
+    
+    $server = "localhost";
+    $user = "narbon";
+    $passwd = "narbon";
+    $db = "narbonn";
+
+    $conn = mysqli_connect($server, $user, $passwd, $db);
+
+    $getip = "SELECT * FROM development";
+    $res = mysqli_query($conn, $getip);
+
+    while ($row = mysqli_fetch_assoc($res)) {
+        $ip = $row['ip'];
+    }
+    
     ?>
 
     <div id="sidebar-nav" class="sidebar">
@@ -8,29 +25,29 @@ function index() {
             <nav>
                 <ul class="nav">
                     <li>
-                        <a href="index.php" class="active"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                        <a href="http://<?php echo $ip; ?>/NarbonSupport/index.php" class="active"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                     </li>
                     <li>
-                        <a href="tickets.php" class=""><i class="fa fa-envelope-o"></i> <span>Tickets</span></a>
+                        <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/tickets.php" class=""><i class="fa fa-envelope-o"></i> <span>Tickets</span></a>
                     </li>
                     <li>
                         <a href="#insub" data-toggle="collapse" class="collapsed"><i class="fa fa-bars"></i> <span>Pro Search</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
                         <div id="insub" class="active collapse ">
                             <ul class="nav">
                                 <li>
-                                    <a href="institute.php" class=""><i class="fa fa-university"></i> <span>Institute</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/institute.php" class=""><i class="fa fa-university"></i> <span>Institute</span></a>
                                 </li>
                                 <li>
-                                    <a href="admin.php" class=""><i class="fa fa-user-plus"></i> <span>Admin</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/admin.php" class=""><i class="fa fa-user-plus"></i> <span>Admin</span></a>
                                 </li>
                                 <li>
-                                    <a href="agent.php" class=""><i class="fa fa-microphone"></i> <span>Agent</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/agent.php" class=""><i class="fa fa-microphone"></i> <span>Agent</span></a>
                                 </li>
                                 <li>
-                                    <a href="teacher.php" class=""><i class="fa fa-book"></i> <span>Teacher</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/teacher.php" class=""><i class="fa fa-book"></i> <span>Teacher</span></a>
                                 </li>
                                 <li>
-                                    <a href="student.php" class=""><i class="fa fa-graduation-cap"></i> <span>Student</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/student.php" class=""><i class="fa fa-graduation-cap"></i> <span>Student</span></a>
                                 </li>
                             </ul>
                         </div>
@@ -44,6 +61,21 @@ function index() {
 }
 
 function tickets() {
+    
+    $server = "localhost";
+    $user = "narbon";
+    $passwd = "narbon";
+    $db = "narbonn";
+
+    $conn = mysqli_connect($server, $user, $passwd, $db);
+
+    $getip = "SELECT * FROM development";
+    $res = mysqli_query($conn, $getip);
+
+    while ($row = mysqli_fetch_assoc($res)) {
+        $ip = $row['ip'];
+    }
+    
     ?>
 
     <div id="sidebar-nav" class="sidebar">
@@ -51,29 +83,29 @@ function tickets() {
             <nav>
                 <ul class="nav">
                     <li>
-                        <a href="index.php" class=""><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                        <a href="http://<?php echo $ip; ?>/NarbonSupport/index.php" class=""><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                     </li>
                     <li>
-                        <a href="tickets.php" class="active"><i class="fa fa-envelope-o"></i> <span>Tickets</span></a>
+                        <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/tickets.php" class="active"><i class="fa fa-envelope-o"></i> <span>Tickets</span></a>
                     </li>
                     <li>
                         <a href="#insub" data-toggle="collapse" class="collapsed"><i class="fa fa-bars"></i> <span>Pro Search</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
                         <div id="insub" class="active collapse ">
                             <ul class="nav">
                                 <li>
-                                    <a href="institute.php" class=""><i class="fa fa-university"></i> <span>Institute</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/institute.php" class=""><i class="fa fa-university"></i> <span>Institute</span></a>
                                 </li>
                                 <li>
-                                    <a href="admin.php" class=""><i class="fa fa-user-plus"></i> <span>Admin</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/admin.php" class=""><i class="fa fa-user-plus"></i> <span>Admin</span></a>
                                 </li>
                                 <li>
-                                    <a href="agent.php" class=""><i class="fa fa-microphone"></i> <span>Agent</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/agent.php" class=""><i class="fa fa-microphone"></i> <span>Agent</span></a>
                                 </li>
                                 <li>
-                                    <a href="teacher.php" class=""><i class="fa fa-book"></i> <span>Teacher</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/teacher.php" class=""><i class="fa fa-book"></i> <span>Teacher</span></a>
                                 </li>
                                 <li>
-                                    <a href="student.php" class=""><i class="fa fa-graduation-cap"></i> <span>Student</span></a>
+                                    <a href="http://<?php echo $ip; ?>/NarbonSupport/pages/search/student.php" class=""><i class="fa fa-graduation-cap"></i> <span>Student</span></a>
                                 </li>
                             </ul>
                         </div>
